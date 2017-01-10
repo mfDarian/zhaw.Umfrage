8a2cdc81255e1088ae45fde111ba30918c0e9819
mfDarian <daniel.langhart@gmail.com>
2017-01-11 00:01:42 +0100

    Add files via upload

--- src/test/java/zhaw/umfrage/test/Main.java
+++ src/test/java/zhaw/umfrage/test/Main.java
@@ -1,11 +1,35 @@
 package zhaw.umfrage.test;
 
+import java.io.File;
+
 import zhaw.umfrage.*;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
+		Survey survey = null;
+		File f = new File("Ernährungsumfrage.ser");
+		try {
+			survey = Survey.getFromFile(f);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		
+		while (true) {
+			SurveyTreeAbstract t = survey.getNextItem();
+			if (t == null) {
+				break;
+			} else {
+				System.out.println(t);
+				Question q = (Question) t.getNextItem();
+				if (q == null) {
+					break;
+				} else {
+					System.out.println(q);
+				}
+			}
+		}
 
 		
 	}
