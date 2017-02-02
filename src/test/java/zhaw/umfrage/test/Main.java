4f07015465ea275e2ead8ca35e6785ed829022a0
mfDarian <daniel.langhart@gmail.com>
2017-02-02 21:25:03 +0100

    Add files via upload

--- src/test/java/zhaw/umfrage/test/Main.java
+++ src/test/java/zhaw/umfrage/test/Main.java
@@ -4,49 +4,33 @@ import java.io.File;
 import java.util.ArrayList;
 
 import zhaw.umfrage.*;
+import zhaw.umfrage.editor.*;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
+		/*
 		Survey survey = null;
-		File f = new File("Ernährungsumfrage.ser");
+		Survey survey2 = null;
+		File f = new File("Vergleichsumfrage.ser");
+		try {
+			survey = Survey.getFromFile(f);
+			survey2 = Survey.getFromFile(f);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		
+		System.out.println(survey.equals(survey2));
+		*/
+		
+		Survey survey = null;
+		File f = new File("Vergleichsumfrage.ser");
 		try {
 			survey = Survey.getFromFile(f);
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
-		
-		while (true) {
-			SurveyTreeAbstract t = survey.getNextItem();
-			if (t == null) {
-				break;
-			} else {
-				System.out.println(t);
-				Question q = (Question) t.getNextItem();
-				if (q == null) {
-					break;
-				} else {
-					System.out.println(q);
-					if (q.toString().equals("Möchten Sie an einer Umfrage zu Ernährung teilnehmen?")) {
-						ArrayList<SurveyTreeAbstract> al = q.getItemList();
-						Answer a = (Answer) al.get(0);
-						System.out.println(a);
-						a.setChosen(true);
-						try {
-							q.setAnswered(true);
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-						System.out.println("Score of Answer: " + a.getScore());
-						System.out.println("Score of Question: " + q.getScore());
-						System.out.println("Score of Questionnaire: " + t.getScore());
-						System.out.println("Score of Survey: " + survey.getScore());
-					}
-				}
-			}
-		}
-
-		
+		//SurveyDetailPanel p = new SurveyDetailPanel(survey);
 	}
 }
